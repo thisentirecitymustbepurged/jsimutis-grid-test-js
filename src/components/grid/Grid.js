579285db6434cf26e47bd0b56fe82902b50e9c37
thisentirecitymustbepurged <yogintas@gmail.com>
2020-01-17 09:55:25 +0000

    extractSameAdjacent

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -2,29 +2,27 @@ import React from 'react';
 import { Cell } from './Cell';
 import './Grid.scss';
 
-const extractSameAdjacent = array => array.reduce((adjacentsByValue, cell, i) => {
+const injectSameAdjacent = (array, key) => array.forEach((adjacentsByValue, cell, i) => {
   const cellPrev = array[i - 1];
   const valuePrev = cellPrev ? cellPrev.value : {};
   const { value } = cell;
 
-  if (!adjacentsByValue[value]) adjacentsByValue[value] = [];
+  // if (!adjacentsByValue[value]) adjacentsByValue[value] = [];
 
-  const adjacents = adjacentsByValue[value];
+  // const adjacents = adjacentsByValue[value];
 
-  if (valuePrev !== value) adjacents.push([]);
+  // if (valuePrev !== value) adjacents.push([]);
 
-  adjacents[adjacents.length - 1].push(cell);
+  // adjacents[adjacents.length - 1].push(cell);
 
-  return adjacentsByValue;
+  // return adjacentsByValue;
 }, {});
 
 const generate = config => {
   const { random, round } = Math;
   const { xLength, yLength, width, height } = config;
-  const matrixes = {
-    xy: [],
-    yx: []
-  };
+  const cols = [];
+  const rows = [];
 
   for (let y = 0; y < yLength; y += 1) {
     for (let x = 0; x < xLength; x += 1) {
@@ -36,22 +34,26 @@ const generate = config => {
         cell.updateView();
       };
 
-      if (matrixes.yx[y]) matrixes.yx[y][x] = cell;
-      else matrixes.yx[y] = [cell];
+      if (cols[x]) cols[x][y] = cell;
+      else cols[x] = [cell];
 
-      if (matrixes.xy[x]) matrixes.xy[x][y] = cell;
-      else matrixes.xy[x] = [cell];
+      if (rows[y]) rows[y][x] = cell;
+      else rows[y] = [cell];
 
-      cell.adjacent = { x: matrixes.xy[x], y: matrixes.yx[y] };
-    }
+      cell.col = cols[x];
+      cell.row = rows[y];
+
+      if (x === xLength) injectAdjacentSame(cell.col, 'col');
+      if (y === yLength) injectAdjacentSame(cell.row, 'row');
   }
 
-  return matrixes;
+
+  return { rows, cols };
 };
 
-const matrixes = generate({ xLength: 10, yLength: 5, width: 50, height: 50 });
+const matrix = generate({ xLength: 10, yLength: 5, width: 50, height: 50 });
 
-window.matrixes = matrixes;
+window.matrix = matrix;
 window.extractSameAdjacent = extractSameAdjacent;
 
-export const Grid = () => <div className="grid">{matrixes.yx.map(arr => arr.map(cell => cell.view))}</div>;
+export const Grid = () => <div className="grid">{matrix.rows.map(arr => arr.map(cell => cell.view))}</div>;
